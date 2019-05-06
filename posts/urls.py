d908cd07a18401b9bd878ef60729e9afd00fc718
Max Mukhin <stch.max.muhin@gmail.com>
2019-05-07 02:12:25 +0300

    chore: add link "Back" to detail post

--- posts/urls.py
+++ posts/urls.py
@@ -4,6 +4,6 @@ from .views import *
 
 
 urlpatterns = [
-    path("", get_posts_list),
+    path("", get_posts_list, name="posts_list_url"),
     path("post/<str:slug>/", get_post_detail, name="post_detail_url"),
 ]
