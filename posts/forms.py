344094243e08a6971535cb5b7bf085b068876b71
Max Mukhin <stch.max.muhin@gmail.com>
2019-07-29 17:02:41 +0300

    chore: code review

--- posts/forms.py
+++ posts/forms.py
@@ -3,17 +3,13 @@ from django.core.exceptions import ValidationError
 
 from .models import Tag
 
-class TagForm(forms.Form):
-    title = forms.CharField(max_length=128)
-    slug = forms.SlugField(max_length=128)
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = ["title", "slug"]
 
     def clean_slug(self):
         slug = self.cleaned_data["slug"].lower()
         if Tag.objects.filter(slug__iexact=slug).count():
             raise ValidationError("Слаг \"{}\" уже существует.".format(slug.lower()))
         return slug
-
-    def save(self):
-        tag = Tag.objects.create(title=self.cleaned_data["title"],
-                                 slug=self.cleaned_data["slug"])
-        return tag
